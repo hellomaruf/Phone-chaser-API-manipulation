e09cfb714a8eaff4ef1afd995f4a078e051a9ed3
maruf-ahmed <marufahamed1110@gmail.com>
2024-02-27 22:04:14 +0600

    added dynamic card by api

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ const loadPhone = async () => {
   );
   let data = await res.json();
   let phones = data.data;
-  console.log(phones);
+  displayCard(phones);
 };
+function displayCard(phones) {
+  let cardContainer = document.getElementById("card-container");
+
+  phones.forEach((element) => {
+    console.log(element);
+    let div = document.createElement("div");
+    div.classList = "card bg-base-100 shadow-md";
+    div.innerHTML = `
+    <figure>
+          <img
+            src="${element.image}"
+            alt="Shoes"
+            class="p-6"
+          />
+        </figure>
+        <div class="card-body text-center">
+          <h2 class="font-semibold text-xl text-center">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions w-full">
+            <button class="btn w-full bg-blue-500 text-white hover:bg-blue-400">
+              Show Details
+            </button>
+          </div>
+        </div>
+    `;
+    cardContainer.appendChild(div);
+  });
+}
 loadPhone();
